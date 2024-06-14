19679dd3813ec198fae475e37cda420bccd5f66b
EmilioPiotto <emilio.piotto@gmail.com>
2024-06-14 09:33:08 -0300

    Multivariate da prep with TSD (2, 72, 36, 4)

--- backend/data_processing_service.py
+++ backend/data_processing_service.py
@@ -1,20 +1,28 @@
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 import torch
+import pandas as pd
+from statsmodels.tsa.seasonal import STL
 
 class DataProcessingService:
-    def __init__(self, seq_length:int = 10,pred_window:int = 1, batch_size:int = 10):
-        """
-        pred_window: (prediction window) how many periods ahead will be forcasted
-        """
+    def __init__(self, seq_length: int = 10, pred_window: int = 1, batch_size: int = 10, has_date_index: bool = True):
         self.seq_length = seq_length
         self.batch_size = batch_size
         self.pred_window = pred_window
+        self.has_date_index = has_date_index
 
     def normalize_data(self, data):
         scaler = MinMaxScaler(feature_range=(-1, 1))
-        data_normalized = scaler.fit_transform(data.reshape(-1, 1))
+        if isinstance(data, pd.Series):
+            data_normalized = scaler.fit_transform(data.values.reshape(-1, 1))
+        else:
+            data_normalized = scaler.fit_transform(data.reshape(-1, 1))
         return data_normalized, scaler
+
+    def decompose_series(self, data_series):
+        stl = STL(data_series)
+        result = stl.fit()
+        return result.observed, result.trend, result.seasonal, result.resid
 
     def create_sequences(self, data):
         xs, ys = [], []
@@ -32,14 +40,24 @@ class DataProcessingService:
         y_train, y_test = y_data[:train_size], y_data[train_size:]
         return x_train, x_test, y_train, y_test
 
+    def process_multiple_series(self, time_series_list):
+        combined_data_list = []
+        for series in time_series_list:
+            normalized_data, _ = self.normalize_data(series)
+            if self.has_date_index and isinstance(series, pd.Series):
+                data_series = pd.Series(normalized_data.squeeze(), index=series.index)
+                observed, trend, seasonal, resid = self.decompose_series(data_series)
+                combined_data = np.stack((observed, trend, seasonal, resid), axis=1)
+            else:
+                combined_data = np.stack((normalized_data.squeeze(),) * 4, axis=1)  # Replicate normalized data for all channels
+            combined_data_list.append(combined_data)
+        return np.array(combined_data_list)
+
     @staticmethod
     def get_sample_data(length):
-        # Linear data with some noise
         start = torch.rand(1).item()
         step = torch.rand(1).item()
-        var = step*4
-
+        var = step * 4
         array = torch.arange(start, start + step * length, step)
         random_values = torch.FloatTensor(length).uniform_(-var, var)
-
         return array + random_values
